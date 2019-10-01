1e538ef790fe169ffc930d1bec0683bc6f31f258
msieg <37848286+msieg@users.noreply.github.com>
2019-10-01 15:04:10 -0400

    Add files via upload

--- deep_visualizer.py
+++ deep_visualizer.py
@@ -1,19 +1,105 @@
 import librosa
-import librosa.display
+import argparse
 import numpy as np
 import moviepy.editor as mpy
 import random
 import torch
 
-from PIL import Image,ImageDraw,ImageFont
 from scipy.misc import toimage
+from tqdm import tqdm
 from pytorch_pretrained_biggan import (BigGAN, one_hot_from_names, truncated_noise_sample,
                                        save_as_images, display_in_terminal)
 
+sys.exit()
 
-#choose model
-#can also use -256 or -128 models to lower resolution and decrease runtime
-model_name='biggan-deep-128'
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--song")
+parser.add_argument("--model_name")
+parser.add_argument("--duration")
+parser.add_argument("--pitch_sensitivity")
+parser.add_argument("--tempo_sensitivity")
+parser.add_argument("--depth")
+parser.add_argument("--classes")
+parser.add_argument("--num_classes")
+parser.add_argument("--jitter")
+parser.add_argument("--hop_length")
+parser.add_argument("--truncation")
+parser.add_argument("--smooth_factor")
+parser.add_argument("--batch_size")
+parser.add_argument("--use_previous_classes")
+parser.add_argument("--use_previous_vectors")
+
+args = parser.parse_args()
+
+if args.song:
+    song=args.song
+    y, sr = librosa.load(song)
+else:
+    raise ValueError("you must enter an audio file name in the --song argument")
+
+if args.model_name:
+    model_name=args.model_name
+else:
+    model_name='biggan-deep-512'
+    
+if args.pitch_sensitivity:
+    pitch_sensitivity=args.pitch_sensitivity
+else:
+    pitch_sensitivity=50  
+    
+if args.tempo_sensitivity:
+    tempo_sensitivity=args.tempo_sensitivity
+else:
+    tempo_sensitivity=0.2
+    
+if args.depth:
+    depth=args.depth
+else:
+    depth=0
+   
+if args.num_classes:
+    num_classes=args.num_classes
+else:
+    num_classes=12
+
+if args.jitter:
+    jitter=args.jitter
+else:
+    jitter=0.5
+
+if args.hop_length:
+    hop_length=args.hop_length
+else:
+    hop_length=512
+    
+if args.truncation:
+    truncation=args.truncation
+else:
+    truncation=1
+    
+if args.smooth_factor:
+    smooth_factor=args.smooth_factor
+else:
+    smooth_factor=10
+    
+if args.batch_size:
+    batch_size=args.batch_size
+else:
+    batch_size=30
+    
+if args.duration:
+    seconds=args.duration
+    frame_lim=int(np.floor(seconds*22050/hop_length/batch_size))
+else:
+    frame_lim=int(np.floor(len(y)/sr*22050/hop_length/batch_size))
+    
+if args.use_previous_vectors:
+    use_previous_vectors=args.use_previous_vectors
+else:
+    use_previous_vectors=0
+
+
 
 # Load pre-trained model
 model = BigGAN.from_pretrained(model_name)
@@ -21,53 +107,6 @@ model = BigGAN.from_pretrained(model_name)
 #set device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-#song file
-song='lastdance.mp3'
-
-#number of seconds of audio to visualize if you want to cuf off early
-seconds=[[300]]
-
-#if you want to repeat the same *starting* class and noise vectors, set to 1
-use_last_vectors=0
-
-#the theme rate controls the pitch sensitivity, i.e. how quickly the classes / themes will update based on changing pitch
-#range: > 2
-#numbers closer to 0 move the theme much faster
-#recommended range: 5 to 200
-theme_rate=40
-
-#how "deep" do you want to travel into the latent space? 
-#numbers closer to 1 keep you close to the surface of your imagenet classes
-#numbers closer to 0 yield more latent structures (lots of dog and human faces)
-#recommended range: 0.2 to 1
-depth=1
-
-#sensitivity of the noise vector to changes in tempo. recommended range: 0.1 to 0.2
-tempo_sensitivity=0.2
-
-#list of 12 imagenet class indices corresponding to 12 pitch class (A, A#, B, C, etc.)
-#see 1000 imagenet class indices here: https://gist.github.com/yrevar/942d3a0ac09ec9e5eb3a
-#if empty, 12 classes will be chosen randomly
-classes=[]
-
-#an integer between 1 and 12 indicating how many pitches / BigGAN classes to incorporate. 
-#if less than 12, priority is given based on overall power in the song (see var chromasort)
-#numbers closer to 1 yield more thematically simple videos.
-num_pitches=12
-
-#number of audio frames per video frame. 512 is standard in librosa. 
-hop_length=512
-
-#BigGAN noise vector truncation. 
-#numbers closer to 1 yield more variable and unique image frames
-truncation = 1
-
-#jitter in the noise vector updates prevents frames from cycling repetiviely during repetitive audio
-#if you do you want to cycle repetiively, set jitter to 0
-jitter=0.5
-
-#smoothing factor: smooth between the means of class vectors of this bin size
-sm=10
 
 
 
@@ -80,7 +119,7 @@ sm=10
 print('\nReading audio \n')
 
 #load song in librosa
-y, sr = librosa.load(song)
+
 
 #create spectrogram
 spec = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128,fmax=8000, hop_length=hop_length)
@@ -118,15 +157,18 @@ chromasort=np.argsort(np.mean(chroma,axis=1))[::-1]
 
 
 #If class list is empty, select 12 random classes
-if not classes:
+if args.classes:
+    classes=args.classes
+else:
     cls1000=list(range(1000))
     random.shuffle(cls1000)
     classes=cls1000[:12]
+    
 
 
 #initialize first class vector
 cv1=np.zeros(1000)
-for p in chromasort[:num_pitches]:
+for p in chromasort[:num_classes]:
     cv1[classes[p]] = chroma[p][0]
 
 
@@ -168,8 +210,11 @@ update_last=np.zeros(128)
 ########################################
 
 
-print('Generating input vectors \n')
-for i in range(len(gradm)):   
+print('\n\nGenerating input vectors \n')
+
+for i in tqdm(range(len(gradm))):   
+    
+    pass
 
     #update jitter vector every 100 frames by setting ~half of noise vector units to lower sensitivity
     if i%100==0:
@@ -222,7 +267,7 @@ for i in range(len(gradm)):
     #initialize new class vector
     cv2=np.zeros(1000)
 
-    for j in range(num_pitches):
+    for j in range(num_classes):
 
         #get class ind
         class_ind=classes[chromasort[j]]
@@ -236,8 +281,8 @@ for i in range(len(gradm)):
         #get current pitch gradient
         add2 = chromagrad[chromasort[j]][i]
   
-        #update class unit based on theme_rate, current pitch power and gradient
-        cv2[class_ind] = (cva + ((add+add2)/theme_rate))/(1+(1/(theme_rate/2)))
+        #update class unit based on pitch_sensitivity, current pitch power and gradient
+        cv2[class_ind] = (cva + ((add+add2)/pitch_sensitivity))/(1+(1/(pitch_sensitivity/2)))
 
 
 
@@ -256,7 +301,7 @@ for i in range(len(gradm)):
         cv2[chromasort[0]]=cv2[chromasort[0]]+0.01
 
     #adjust depth    
-    cv2=cv2*depth
+    cv2=cv2*(1-depth)
     
     #append new class vector
     class_vectors.append(cv2)
@@ -271,23 +316,24 @@ for i in range(len(gradm)):
 #interpolate between class vector bins to to smooth frames
 class_vectors_terp=[]
 
-for c in range(int(np.floor(len(class_vectors)/sm)-1)):
+for c in range(int(np.floor(len(class_vectors)/smooth_factor)-1)):
 
-    ci=c*sm
+    ci=c*smooth_factor
         
-    cva=np.mean(class_vectors[int(ci):int(ci)+sm],axis=0)
-    cvb=np.mean(class_vectors[int(ci)+sm:int(ci)+sm*2],axis=0)
+    cva=np.mean(class_vectors[int(ci):int(ci)+smooth_factor],axis=0)
+    cvb=np.mean(class_vectors[int(ci)+smooth_factor:int(ci)+smooth_factor*2],axis=0)
                 
-    for j in range(sm):
+    for j in range(smooth_factor):
                                 
-        cvc = cva*(1-j/(sm-1)) + cvb*(j/(sm-1))
+        cvc = cva*(1-j/(smooth_factor-1)) + cvb*(j/(smooth_factor-1))
                                         
         class_vectors_terp.append(cvc)
 
 class_vectors=np.array(class_vectors_terp)
 
 
-if use_last_vectors==1:   
+if use_previous_vectors==1:   
+    #load vectors from previous run
     class_vectors=np.load('class_vectors.npy')
     noise_vectors=np.load('noise_vectors.npy')
     
@@ -304,11 +350,9 @@ class_vectors = torch.Tensor(np.array(class_vectors))
 
 
 
-#Generate frames in batches of 30
+#Generate frames in batches of batch_size
 
-batch_size=30
-
-print('Generating frames \n')
+print('\n\nGenerating frames \n')
 
 #send to CUDA if running on GPU
 model=model.to(device)
@@ -316,22 +360,13 @@ noise_vectors=noise_vectors.to(device)
 class_vectors=class_vectors.to(device)
 
 
-
-
-if isinstance(seconds,list)==False:
-    frame_lim=int(np.floor(seconds*22050/512/batch_size))
-else:
-    frame_lim=999
-    
 frames = []
-frame_ind=-batch_size
 
-for i in range(frame_lim):
-    
-    frame_ind=i*batch_size
 
-    print(str(np.round(frame_ind/len(noise_vectors)*100,2)) + '% complete')
+for i in tqdm(range(frame_lim)):
     
+    pass
+
     if (i+1)*batch_size > len(class_vectors):
         break
     
@@ -355,14 +390,10 @@ for i in range(frame_lim):
 
 
 
-#Save video
-        
+#Save video  
 aud = mpy.AudioFileClip(song,fps = 44100) 
-
 clip = mpy.ImageSequenceClip(frames, fps=22050/hop_length)
-
 clip = clip.set_audio(aud)
-
 clip.write_videofile("output.mp4",audio_codec='aac')
 
 
